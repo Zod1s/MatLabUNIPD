3aa33e677357b4c69a040e995ec5bb8ae2323435
Zod1s <lorefrance15@gmail.com>
2023-03-23 16:17:19 +0100

    ex3

--- LabAuto/LabNumerico1/ex3.m
+++ LabAuto/LabNumerico1/ex3.m
@@ -8,7 +8,7 @@ rads2rpm = 60 / (2 * pi); %[rpm * s/rad]
 rpm2rads = 2 * pi / 60; %[rad/(s * rpm)]
 mot.k = 5.2;
 mot.T = 0.03;
-Kp = 0.744;
+Kp = 0.7442;
 w.t = 0.1; %[s]
 w.A = 300 * rpm2rads; %[rad/s]
 d.t = 0.5; %[s]
@@ -17,13 +17,67 @@ diff.wc = 2 * pi * 50;
 diff.d = 1 / sqrt(2);
 w0 = 0; %[rad/s]
 
-open_system("motore.slx");
+%% Open the model
+open_system("motore_P.slx");
 
-%% System simulation
-set_param("motore", "SolverType", "Variable-step", ...
+%% Simulating the model
+set_param("motore_P", "SolverType", "Variable-step", ...
+    "Solver", "ode45", "MaxStep", "0.001", "StopTime", "1");
+
+sim("motore_P");
+
+%% Plotting results
+figure(1)
+subplot(2, 1, 1)
+hold on
+grid on
+plot(w_star.time, w_star.data * rads2rpm)
+plot(w.time, w.data * rads2rpm)
+plot(wm.time, wm.data * rads2rpm)
+legend("w^{*}", "w", "w_{m}")
+
+subplot(2, 1, 2)
+hold on
+grid on
+plot(u.time, u.data)
+legend("u")
+
+%% Open the model
+open_system("motore_PI.slx");
+
+%% Simulating the model
+set_param("motore_PI", "SolverType", "Variable-step", ...
     "Solver", "ode45", "MaxStep", "0.001", "StopTime", "5");
 
-sim("motore");
+sim("motore_PI");
+
+%% Plotting results
+figure(1)
+subplot(2, 1, 1)
+hold on
+grid on
+plot(w_star_i.time, w_star_i.data * rads2rpm)
+plot(w_i.time, w_i.data * rads2rpm)
+plot(wm_i.time, wm_i.data * rads2rpm)
+legend("w^{*}", "w", "w_{m}")
+
+subplot(2, 1, 2)
+hold on
+grid on
+plot(u_i.time, u_i.data)
+legend("u")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
