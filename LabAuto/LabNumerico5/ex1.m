592e84b92f37c6d955175ebbb2299e7c0d3c6586
Zod1s <lorefrance15@gmail.com>
2023-04-20 23:48:21 +0200

    all

--- LabAuto/LabNumerico5/ex1.m
+++ LabAuto/LabNumerico5/ex1.m
@@ -5,7 +5,12 @@ clc
 
 %% Parameters
 load("params.mat")
+Beq = 1.28e-6; %[Nm/(rad/s)] Beq = Bm + Bl / N^2
+Jeq = mld.J / gbox.N^2 + mot.J; 
+tausf = 1e-2; %[Nm]
 
+%% Simulink model
+open_system("motor1.slx")
 
 
 
